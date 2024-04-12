a836a27b048238637037c33ff6dffefeb753c326
mbanik2117 <153079956+mbanik2117@users.noreply.github.com>
2024-04-12 13:36:34 +0530

    Delete build_files.sh

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install required packages and dependencies
-pip install -r requirements.txt
-
-# Collect static files
-python3.10 manage.py collectstatic --noinput
-python3.10 manage.py migrate --noinput
