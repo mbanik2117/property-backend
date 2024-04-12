61726a7e6904d1646278fc25506214b1e788c975
mbanik2117 <153079956+mbanik2117@users.noreply.github.com>
2024-04-12 13:22:31 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 
 pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput
-python3.10 manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
