07c09fecb4faa24b340418deae5d2a63ffd340f8
mbanik2117 <153079956+mbanik2117@users.noreply.github.com>
2024-04-12 13:09:54 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
