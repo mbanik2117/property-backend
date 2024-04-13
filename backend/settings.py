81d4952d6efc9cdd080acfebf0fab3cf51c75765
mbanik2117 <153079956+mbanik2117@users.noreply.github.com>
2024-04-13 21:03:04 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -180,5 +180,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("HOST_PASSWORD")
 
-
+SECURE_SSL_REDIRECT = \
+    os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
+if SECURE_SSL_REDIRECT:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
