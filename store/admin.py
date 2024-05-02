d1bc03ec00dd6861ae5a3bf20d9e9d346e653cfb
mbanik2117 <153079956+mbanik2117@users.noreply.github.com>
2024-05-02 08:33:15 +0530

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Property, PropertyImage, PropertyVideo
+from .models import Property, PropertyImage, PropertyVideo, ShortlistedProperty
 
 
 class PropertyImageInline(admin.TabularInline):
